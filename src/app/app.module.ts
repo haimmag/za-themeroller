f9c407f34b2bb15134e9eedbc395b37c133b7f98
Dekel Yaacov <dekely@checkpoint.com>
2017-07-05 18:49:58 +0300

    changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,29 @@
+import { NgModule, Type } from '@angular/core';
+import { BrowserModule, Title }  from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { CovalentHttpModule, IHttpInterceptor } from '@covalent/http';
+import { CovalentHighlightModule } from '@covalent/highlight';
+import { CovalentMarkdownModule } from '@covalent/markdown';
+
+import { AppComponent } from './app.component';
+import { RequestInterceptor } from '../config/interceptors/request.interceptor';
+import { MOCK_API } from '../config/api.config';
+
+import { routedComponents, AppRoutingModule } from './app-routing.module';
+
+import { SharedModule } from './shared/shared.module';
+
+import { USER_PROVIDER, USERS_API } from './users';
+import {ColorPickerModule} from 'ngx-color-picker';
+import { IosLayoutComponent } from './logsp/components/ios-layout/ios-layout.component';
+import { AndroidLayoutComponent } from './logsp/components/android-layout/android-layout.component';
+import { MainPageComponent } from './logsp/components/main-page/main-page.component';
+import { MdRadioModule } from '@angular/material';
+import { RiskPageComponent } from './logsp/components/risk-page/risk-page.component';
+import { GenerateComponent } from './logsp/components/generate/generate.component';
 import { CovalentDialogsModule } from '@covalent/core';
 import { CovalentDynamicFormsModule } from '@covalent/dynamic-forms';
-import { RiskPageComponent } from './logsp/components/risk-page/risk-page.component';
 
 const httpInterceptorProviders: Type<any>[] = [
   RequestInterceptor,
