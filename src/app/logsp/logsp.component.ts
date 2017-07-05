420d25562d4406ace9f5199233235213b1239762
haimma <haimma@checkpoint.com>
2017-07-05 11:49:42 +0300

    Initial commit

---
+++ src/app/logsp/logsp.component.ts
@@ -0,0 +1,39 @@
+import { Component, AfterViewInit, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Title }     from '@angular/platform-browser';
+
+import { TdLoadingService, TdMediaService } from '@covalent/core';
+
+import { ItemsService, ProductsService } from '../../services';
+
+@Component({
+  selector: 'qs-logsp',
+  templateUrl: './logsp.component.html',
+  styleUrls: ['./logsp.component.scss'],
+  viewProviders: [ ItemsService, ProductsService ],
+})
+export class LogspComponent implements AfterViewInit, OnInit {
+
+  color: string = '#127bdc';
+
+  constructor(private _titleService: Title,
+              private _itemsService: ItemsService,
+              private _productsService: ProductsService,
+              private _loadingService: TdLoadingService,
+              private _changeDetectorRef: ChangeDetectorRef,
+              public media: TdMediaService) {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngAfterViewInit(): void {
+    // broadcast to all listener observables when loading the page
+    this.media.broadcast();
+    // force a new change detection cycle since change detections
+    // have finished when `ngAfterViewInit` is executed
+    this._changeDetectorRef.detectChanges();
+  }
+
+}
